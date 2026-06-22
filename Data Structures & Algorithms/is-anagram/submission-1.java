b2150f10a3fe8b5c687791c3cd795d651959321e
abhinav <abhnvashgpta@gmail.com>
2026-06-22 10:02:01 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()) return false;
+
+        int[] count = new int[26];
+
+        for(char ch : s.toCharArray()) {
+            ++count[ch - 'a'];
+        }
+
+        for(char ch : t.toCharArray()) {
+            --count[ch - 'a'];
+        }
+
+        for(int i : count) {
+            if(i != 0) return false;
+        }
+
+        return true;
+    }
+}
